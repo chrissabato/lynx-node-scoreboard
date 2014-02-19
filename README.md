a1ad79686cfe2f7c0c384664b605512052c974f4
Christopher Sabato <chris@chrissabato.com>
2014-02-18 20:08:26 -0800

    Cleaning up code and updating README

--- README.md
+++ README.md
@@ -5,17 +5,34 @@ Finishlynx Node.js web scoreboard
 Node Setup
 --------------------
 + install node.js
-+ add node to PATH
++ add node to PATH (windows installed doen't do this by default)
+  + open cmd prompt as administrator
+  + SET PATH=C:\Program Files\Nodejs;%PATH%
 + create a folder for you Node project (c:\node)
 + install socket.io node.js module 
   + must be in the same directory as the node project
-
+  + from a command prompt
+    + CD c:\node
+    + npm install socket.io
+    
 FinishLynx Setup
 --------------------
 + copy nodejs.lss into the Finishlynx directory
 + setup scoreboard in FinishLynx
-
-Run
+  + Script: nodejs.lss
+  + Code Set: Single Byte
+  + Serial Port: Network (UDP)
+  + Port: 43278
+  + IP Address: 255.255.255.255
+  + Running Time: Off
+  + Results: Auto
+  + Alway send place: checked
+  + Paging: checked
+  + Size: 24
+  + Max: 24
+  + Include first name: checked
+  
+Run Server
 --------------------
 + start the node upd/http server
   + node lynx.js
--- lynx.js
+++ lynx.js
@@ -1,49 +1,71 @@
+/*  
+************************************
+** Node.js requirments            **
+************************************
+*/
 var dgram = require("dgram");
 var udpServer = dgram.createSocket("udp4");
 var http = require('http');
 var fs = require('fs');
 var io = require('socket.io');
 
+
+// Defining some variables
 var htmlPage;
 var jsonResults;
 var tempResults = '';
 var finalResults = '';
-udpServer.on("message", function (msg, rinfo) { 
-    if(rinfo.size == 536){
-        tempResults = tempResults + msg.toString();
-    } 
-    else{
-        finalResults = tempResults + msg.toString();
-        
-            r = finalResults.split(';');
-            for (var i = 0; i < r.length; i++) {          
-                var tmp = r[i].length;
-                if (tmp == 14 || tmp == 0){
-                    r.splice(i,1);
-                }
-                else {
-                    //console.log(r[i]);
-                    r[i]= r[i].split(',');
-                    
-                }
-            }
-            jsonResults = JSON.stringify(r);
-        tempResults ='';
-    }
-});
-
-udpServer.on("listening", function () {
-  var address = udpServer.address();
-  console.log("UDP server listening " + address.address + ":" + address.port);
-});
-udpServer.bind(43278);
 
 
 /*  
-** Webserver and sockets.io
+************************************
+** UDP Server                     **
+************************************
+*/
+
+// start UDP server listening on port 43278
+udpServer.on("listening", function () {
+    var address = udpServer.address();
+    console.log("UDP server listening on port: " + address.port);
+});
+udpServer.bind(43278);
+
+// process datagram
+udpServer.on("message", function (msg, rinfo) { 
+    // check if datagram is full (size=536)
+    if(rinfo.size == 536){
+        tempResults = tempResults + msg.toString();
+    } 
+    // datagram is not full, so this is the end of the message
+    else{
+        finalResults = tempResults + msg.toString();
+        
+        r = finalResults.split(';');            // split the final string by ';' and put in array r
+        for (var i = 0; i < r.length; i++) {    // loop through each array element (result line)      
+            var tmp = r[i].length;              // check size, if it's 14 it is an empty result set
+            if (tmp == 14 || tmp == 0){
+                r.splice(i,1);                  // if empty remove from array
+            }
+            else {
+                //console.log(r[i]);
+                r[i]= r[i].split(',');          // if line is not empty split the line bt ','
+                
+            }
+        }
+        jsonResults = JSON.stringify(r);        // convert final array into a json string
+        tempResults ='';                        // reset tempResults variable to prepare for next datagram
+    }
+});
+
+
+/*  
+************************************
+** Webserver and sockets.io       **
+************************************
 */
 var htmlPage;
 
+// read html file to serve
 fs.readFile('lynx.html', function(error, data){
     if (error){
         throw error;
@@ -53,18 +75,21 @@ fs.readFile('lynx.html', function(error, data){
     }
 });
 
-
+// start webserver 
 var htmlServer = http.createServer(function(request, response) {
     response.writeHeader(200, {"Content-Type": "text/html"});
     response.end(htmlPage);
 });
 
-htmlServer.listen(8000)
-var serv_io = io.listen(htmlServer, { log: false });
+htmlServer.listen(8000);
+console.log("HTML Server started");
 
+// start up socket.io for transmitting data to the webpage
+var serv_io = io.listen(htmlServer, { log: false });
 serv_io.sockets.on('connection', function(socket){
     //send data to client
     setInterval(function(){
         socket.send(jsonResults);
     }, 1000);
 });
+
